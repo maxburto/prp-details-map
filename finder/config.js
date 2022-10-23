0cd4e11e615768ab88e47c1976df4d91d6e02311
Maxwell Burton <maxburto@gmail.com>
2022-10-22 23:13:44 -0700

    Update config.js

--- finder/config.js
+++ finder/config.js
@@ -2,18 +2,29 @@
 
 // eslint-disable-next-line no-unused-vars
 const config = {
-  style: 'mapbox://styles/mapbox/light-v10',
+  style: 'mapbox://styles/mapbox/outdoors-v11',
   accessToken:
     'pk.eyJ1IjoibWF4YnVydG8iLCJhIjoiY2t5MHZwNnI2MDU1bjJ2cGUwYXhqYndpbCJ9.31g_YC48d2c_HDyPD_XhUA',
   CSV: 'https://docs.google.com/spreadsheets/d/1TCGYI8tr8qI7Rtcqu-XqobbYHEqin9BpKjNGTKzD9PE/gviz/tq?tqx=out:csv&sheet=data',
-  center: [-120.234, 47.398],
-  zoom: 6,
-  title: 'Replace with your title',
+  center: [-122.335126, 47.640467],
+  zoom: 10,
+  title: 'PRP drop-off details map',
   description:
     'Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.',
   sideBarInfo: ['Client Name', 'Address', 'Phone'],
   popupInfo: ['Client Name'],
   filters: [
+     {
+      type: 'dropdown',
+      title: 'Route: ',
+      columnHeader: 'Route',
+      listItems: [
+        'New volunteer orientation',
+        'Solo - Solo deliveries and pantry restocks',
+        'Group - Ravenna route - Meet for coffee after',
+        'Group - University District route - Meet for coffee aft',
+      ],
+    },
     {
       type: 'checkbox',
       title: 'Location type: ',
