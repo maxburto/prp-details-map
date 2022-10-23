37c4c7064894909f9171edc01ac4c0bf6979f856
Maxwell Burton <maxburto@gmail.com>
2022-10-22 22:28:46 -0700

    Update config.js

--- finder/config.js
+++ finder/config.js
@@ -11,8 +11,8 @@ const config = {
   title: 'Replace with your title',
   description:
     'Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.',
-  sideBarInfo: ['Location_Name', 'Address', 'Phone'],
-  popupInfo: ['Location_Name'],
+  sideBarInfo: ['Location Name', 'Address', 'Phone'],
+  popupInfo: ['Location Name'],
   filters: [
     {
       type: 'dropdown',
