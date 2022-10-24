9e2339500a84f863949fedaa5d115751acc54b7f
Maxwell Burton <maxburto@gmail.com>
2022-10-24 16:58:50 -0700

    Update app.js

--- finder/app.js
+++ finder/app.js
@@ -518,7 +518,7 @@ map.on('load', () => {
 
           data.features.forEach((feature) => {
             console.log("data.features: "+feature);
-            console.log(feature);
+            console.log(feature.properties);
             if (feature.properties.CampaignId === "UDFB") {
                 filteredGeojson.features.push(feature);
               }
